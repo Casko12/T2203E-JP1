d6b55e2397813917f4e08f0000c3cc6866a67551
Casko12 <phamduyhung.ftu@gmail.com>
2022-09-17 18:49:13 +0700

    JP1

--- src/Session1/Car.java
+++ src/Session1/Car.java
@@ -1,7 +1,9 @@
 package Session1;
 
 public class Car {
-    String brand = "BMW";
+    public String brand = "BMW"; //private String... thi chi ban than no dung duoc, dung khi no la lop cuoi cung tren cay pha he
+    //default: chi package cua no dung dc
+    //protected thi chi co no dung dc va cac related voi no dung duoc
     int year = 2022;
 
     static String color;
--- src/Session1/PhanSo.java
+++ src/Session1/PhanSo.java
@@ -1,37 +1,58 @@
 package Session1;
 
 public class PhanSo {
-    int tuso;
-    int mauso;
+   public int tuSo;
+   public int mauSo;
 
     public PhanSo(){};
 
     public int getTuso() {
-        return tuso;
+        return tuSo;
     }
 
     public void setTuso(int tuso) {
-        this.tuso = tuso;
+        this.tuSo = tuSo;
     }
 
     public int getMauso() {
-        return mauso;
+        return mauSo;
     }
 
     public void setMauso(int mauso) {
-        this.mauso = mauso;
-    }
-
-    public boolean ktPhanSo(){
-        return mauso !=0;
-    }
-
-    public int UCLN(){
-        if(! ktPhanSo()) return 0;
-
+        this.mauSo = mauSo;
     }
 
 
+    public void rutGon() {
 
+        int UCLN = 1;
+        for (int i = Math.min(Math.abs(tuSo), Math.abs(mauSo)); i > 1; i--) {
+            if (tuSo % i == 0 && mauSo % i == 0) {
+                UCLN = i;
+                break;
+            }
+        }
+        setTuso(tuSo/UCLN);
+        setMauso(mauSo/UCLN);
+    }
+    public void inPhanSo(){
+        System.out.println(getTuso()+"/"+getMauso());
+    }
 
+    public void nghichDao(){
+
+    }
+
+    public void add(){
+
+    }
+    public void sub(){
+
+    }
+    public void mul(){
+
+    }
+    public void div(){
+
+    }
 }
