f5ba7847fa3cd1252d3d75b45f3d0a49fdffa0ff
Casko12 <phamduyhung.ftu@gmail.com>
2022-09-17 18:56:35 +0700

    JP1

--- src/Session1/PhanSo.java
+++ src/Session1/PhanSo.java
@@ -26,7 +26,7 @@ public class PhanSo {
     public void rutGon() {
 
         int UCLN = 1;
-        for (int i = Math.min(Math.abs(tuSo), Math.abs(mauSo)); i > 1; i--) {
+        for (int i = Math.min(Math.abs(tuSo), Math.abs(mauSo)); i > 1; i--) { //co the thay tuSo bang getTuso
             if (tuSo % i == 0 && mauSo % i == 0) {
                 UCLN = i;
                 break;
@@ -40,11 +40,17 @@ public class PhanSo {
     }
 
     public void nghichDao(){
-
+    int tmp = tuSo;
+    tuSo = mauSo;
+    mauSo = tmp;
     }
 
-    public void add(){
-
+    public PhanSo add(PhanSo f){
+        PhanSo tong = new PhanSo();
+        tong.tuSo = this.tuSo*f.mauSo + this.mauSo*f.tuSo;
+        tong.mauSo = this.mauSo*f.mauSo;
+        tong.rutGon(); //rut gon tong sau khi cong
+        return tong;
     }
     public void sub(){
 
