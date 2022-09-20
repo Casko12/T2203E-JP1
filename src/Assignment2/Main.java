65c0d2e97a2b232da6390738f6bf0814eb794c44
Casko12 <phamduyhung.ftu@gmail.com>
2022-09-20 18:35:16 +0700

    JP1

--- src/Assignment2/Main.java
+++ src/Assignment2/Main.java
@@ -7,15 +7,17 @@ public class Main {
         Account myAccount = new Account("123456789","Pham Duy Hung",5000000);
         Account myAccount2 = new Account ("987654321","Tran Van Thanh",10000000);
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap so tien muon rut");
+        System.out.println("Nhap so tien muon rut:");
         int x = sc.nextInt();
+        System.out.println("Nhap so tien muon chuyen:");
+        int y = sc.nextInt();
         if(myAccount.debit(x)){
-            System.out.println("Rut thanh cong"+x);
+            System.out.println("Rut thanh cong "+x);
         }else{
             System.out.println("So du tai khoan khong du hoac nhap sai so tien");
         }
-        if(myAccount.transferTo(myAccount2, 3000000)){
-            System.out.println("Chuyen thanh cong sang tai khoan"+myAccount2.getName());
+        if(myAccount.transferTo(myAccount2, y)){
+            System.out.println("Chuyen thanh cong sang tai khoan "+myAccount2.getName());
         }else{
             System.out.println("Chuyen khoan that bai");
         }
