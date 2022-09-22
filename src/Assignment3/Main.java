fe11453112df156489c364ba63149490f488452c
Casko12 <phamduyhung.ftu@gmail.com>
2022-09-22 18:51:12 +0700

    JP1

--- src/Assignment3/Main.java
+++ src/Assignment3/Main.java
@@ -2,7 +2,9 @@ package Assignment3;
 
 public class Main {
     public static void main(String[] args) {
-
+        KhachVietNam kh1 = new KhachVietNam(111,"Pham Duy Hung","2022-09-21",130,"Ca nhan");
+        KhachNuocNgoai kh2 = new KhachNuocNgoai(222,"John Wick","2022-09-22",312,"USA");
+        System.out.println(kh1.thanhTien());
+        System.out.println(kh2.thanhTien());
     }
-
 }
