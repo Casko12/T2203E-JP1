6031fa89dd127ba71520de754d702e12072561da
Casko12 <phamduyhung.ftu@gmail.com>
2022-09-22 18:46:21 +0700

    JP1

--- src/Assignment3/KhachNuocNgoai.java
+++ src/Assignment3/KhachNuocNgoai.java
@@ -1,5 +1,27 @@
 package Assignment3;
 
 public class KhachNuocNgoai extends KhachHang {
+    public String quocTich;
 
+    public KhachNuocNgoai(String quocTich) {
+        this.quocTich = quocTich;
+    }
+
+    public KhachNuocNgoai(int id, String name, String date, int quantity, String quocTich) {
+        super(id, name, date, quantity);
+        this.quocTich = quocTich;
+    }
+
+    public String getQuocTich() {
+        return quocTich;
+    }
+
+    public void setQuocTich(String quocTich) {
+        this.quocTich = quocTich;
+    }
+
+    @Override
+    public int thanhTien() {
+        return super.thanhTien();
+    }
 }
