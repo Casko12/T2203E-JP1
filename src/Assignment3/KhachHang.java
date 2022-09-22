86cd1f4c09b76234591c3c76ed83bfcb2058cf01
Casko12 <phamduyhung.ftu@gmail.com>
2022-09-22 18:29:32 +0700

    JP1

--- src/Assignment3/KhachHang.java
+++ src/Assignment3/KhachHang.java
@@ -7,7 +7,45 @@ public class KhachHang {
     }
     public int id;
     public String name;
+    public String date;
+    public int quantity;
 
-    public int amount;
+    public KhachHang(int id, String name, String date, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.quantity = quantity;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
