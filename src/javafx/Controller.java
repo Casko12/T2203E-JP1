a34184d8f861f8501e6a01b90369bd212dc8be78
Casko12 <phamduyhung.ftu@gmail.com>
2022-10-01 20:54:57 +0700

    JP1

--- src/javafx/Controller.java
+++ src/javafx/Controller.java
@@ -12,14 +12,46 @@ public class Controller {
     public Text txEmail;
     public Text txAge;
     public Text txAddress;
+    public Text noticetxFullName;
+    public Text noticetxEmail;
+    public Text noticetxAge;
+    public Text noticetxAddress;
+
     public void handleSubmit(){
         String fn = txtFullName.getText();
-        txFullName.setText(fn);
+        if(fn.isEmpty()){
+            noticetxFullName.setText("Vui lòng nhập họ tên!");
+            noticetxFullName.setVisible(true);
+        }else {
+            txFullName.setText(fn);
+            noticetxFullName.setVisible(false);
+        }
         String em = txtEmail.getText();
-        txEmail.setText(em);
+        if(em.isEmpty() || !em.contains("@") || em.startsWith("@") || em.endsWith("@")){ //phai chứa @, @ k dc o dau va k dc o cuoi
+            noticetxEmail.setText("Vui lòng nhập Email hợp lệ!");
+            noticetxEmail.setVisible(true);
+        }else {
+            txEmail.setText(em);
+            noticetxEmail.setVisible(false);
+        }
         String ag = txtAge.getText();
-        txAge.setText(ag);
+        try {
+            Integer nAge = Integer.parseInt(ag);
+            if(nAge <= 0 || nAge>100)
+                throw new Exception("Vui lòng nhập tuổi hợp lệ");
+            txAge.setText(nAge.toString());
+            noticetxAge.setVisible(false);
+        }catch (Exception e){
+            noticetxAge.setText(e.getMessage());
+            noticetxAge.setVisible(true);
+        }
         String ad = txtAddress.getText();
-        txAddress.setText(ad);
+        if(ad.isEmpty()){
+            noticetxAddress.setText("Vui lòng nhập địa chỉ!");
+            noticetxAddress.setVisible(true);
+        }else {
+            txAddress.setText(ad);
+            noticetxAddress.setVisible(false);
+        }
     }
 }
