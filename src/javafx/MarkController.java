5231b25af7924c827b88346a9aa9dc791032b0d0
Casko12 <phamduyhung.ftu@gmail.com>
2022-10-04 00:15:53 +0700

    JP1

--- src/javafx/MarkController.java
+++ src/javafx/MarkController.java
@@ -1,14 +1,34 @@
 package javafx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
 
 import java.awt.*;
 
 public class MarkController {
-    public TextField txtName;
-    public TextField txtEmail;
-    public TextField txtMark;
+    public TextField txtStName;
+    public TextField txtStEmail;
+    public TextField txtStMark;
     public ListView<Student> st;
     public Text error;
+    private ObservableList<PhoneNumber> studentList = FXCollections.observableArrayList();
+
+    public void addStudent(){
+        try{
+            error.setVisible(false);
+            if(txtStName.getText().isEmpty() || txtStEmail.getText().isEmpty() || txtStMark.getText().isEmpty()){
+                throw new Exception("Vui lòng nhập đủ tên, email và điểm");
+            }
+            printResult();
+        }catch (Exception e){
+            error.setText(e.getMessage());
+            error.setVisible(true);
+        }
+    }
+    public void printResult(){
+        st.setItems(studentList);
+        st.refresh();
+    }
 }
--- src/javafx/Student.java
+++ src/javafx/Student.java
@@ -1,4 +1,43 @@
 package javafx;
 
 public class Student {
+    String name;
+    String email;
+    String mark;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public Student(String name, String email, String mark) {
+        this.name = name;
+        this.email = email;
+        this.mark = mark;
+    }
+    public String toString(){
+        return getName()+"-"+getEmail()+"-"+getMark()+"\n";
+    }
 }
