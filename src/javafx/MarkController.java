a51ec838d42d549fcb7c6b7ca4287a16c5a7e81c
Casko12 <phamduyhung.ftu@gmail.com>
2022-10-04 19:51:15 +0700

    JP1

--- src/javafx/MarkController.java
+++ src/javafx/MarkController.java
@@ -2,23 +2,36 @@ package javafx;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+
+import java.net.URL;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.ResourceBundle;
 
 
-public class MarkController {
+public class MarkController implements Initializable {
     public TextField txtName;
     public TextField txtEmail;
     public TextField txtMark;
     public ListView<Student> st;
     public Text error;
+    public Button sortByName;
+    public Button sortByMark;
     private boolean sortName = true;
     private boolean sortMark = true;
     private ObservableList<Student> studentList = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        studentList.add(new Student("Hà Hoàng Hưng", "hunghh@gmail.com", 1));
+        studentList.add(new Student("Trần Văn Thành", "thanhtv@gmail.com", 2));
+        st.setItems(studentList);
+    }
 
     public void addStudent(){
         try{
@@ -49,6 +62,7 @@ public class MarkController {
                 return sortName?o1.getName().compareTo(o2.getName()):o2.getName().compareTo(o1.getName());
             }
         });
+        sortByName.setText(sortName?"Z->A":"A->Z");
         sortName = !sortName;
         st.refresh();
     }
@@ -59,6 +73,7 @@ public class MarkController {
                 return sortMark?o1.getMark() - o2.getMark():o2.getMark()- o1.getMark();
             }
         });
+        sortByMark.setText(sortMark?"High to Low":"Low to High");
         sortMark = !sortMark;
         st.refresh();
     }
