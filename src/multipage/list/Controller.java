85f82bcb244e58730c899c8f66c426edea915ba5
Casko12 <phamduyhung.ftu@gmail.com>
2022-10-05 23:31:10 +0700

    JP1

--- src/multipage/list/Controller.java
+++ src/multipage/list/Controller.java
@@ -4,12 +4,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import multipage.Main;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
 
     public void goToForm(ActionEvent actionEvent) throws Exception {
         Parent listPage = FXMLLoader.load(getClass().getResource("../form/Form.fxml"));
@@ -21,4 +25,10 @@ public class Controller {
     public ListView <SubjectList> lv;
     private ObservableList<SubjectList> subjectList = FXCollections.observableArrayList();
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        subjectList.add(new SubjectList("001","Java Programing 1","17h30"));
+        subjectList.add(new SubjectList("002","HTML CSS","17h30"));
+        lv.setItems(subjectList);
+    }
 }
